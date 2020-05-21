701ee54b55ee6c271df2b51d5007671b1da24e36
Lorenzo Zenitsky <lorenzoz@iastate.edu>
2020-05-21 16:22:33 -0500

    worked on sider and bottom nav

--- ipaws-app/App.tsx
+++ ipaws-app/App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import {
   Container,
@@ -16,6 +17,7 @@ import {
   CardItem,
   Body
 } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
 
 import Sidebar from './components/Sidebar';
 
@@ -26,94 +28,126 @@ export default class App extends Component {
   };
   render() {
     return (
-      <Container>
-        <Drawer
-          content={<Sidebar />}
-          open={this.state.showSidebar}
-          onClose={() => this.setState({ showSidebar: false })}
-        >
-          <Header
-            style={{
-              backgroundColor: '#1ABC9C'
-            }}
-            androidStatusBarColor="#1ABC9C"
-            hasSubtitle={true}
-          >
-            <Left>
-              <Button
-                rounded
-                style={{ backgroundColor: '#1ABC9C' }}
-                onPress={() =>
-                  this.setState({ showSidebar: !this.state.showSidebar })
-                }
-              >
-                <Icon name="menu" color="#E8E8E8" />
-              </Button>
-            </Left>
-            <Right>
-              <Title
-                style={{
-                  fontSize: 24,
-                  fontWeight: 'bold',
-                  color: '#E8E8E8',
-                  alignSelf: 'center'
-                }}
-              >
-                Idyllic Paws
-              </Title>
-            </Right>
-          </Header>
-          <Content
-            padder
-            style={{ alignContent: 'center', backgroundColor: '#A9E6DA' }}
-          >
-            <Card>
-              <CardItem header bordered>
-                <Text>Holistic Wellness for the Love of Cats & Dogs</Text>
-              </CardItem>
-              <CardItem bordered>
-                <Body>
-                  <Text>
-                    Welcome to Idyllic Paws a house-call based, small animal
-                    practice dedicated to your animal companions and a holistic,
-                    individualized approach in patient healthcare. I’ve helped
-                    many dogs and cats overcome illnesses, allergies, and other
-                    immune disorders, and go on to lead happy, healthy lives.
-                  </Text>
-                </Body>
-              </CardItem>
-              <CardItem footer bordered>
-                <Text>Dr. Zenitsky looks forward to working with you!</Text>
-              </CardItem>
-            </Card>
-          </Content>
-          <Footer>
-            <FooterTab
-              style={{
-                backgroundColor: '#1ABC9C',
-                padding: 5
-              }}
+      <NavigationContainer>
+        {
+          <Container>
+            <Drawer
+              content={<Sidebar />}
+              open={this.state.showSidebar}
+              onClose={() => this.setState({ showSidebar: false })}
             >
-              <Button vertical>
-                <Icon name="home" />
-                <Text>Home</Text>
-              </Button>
-              <Button vertical>
-                <Icon name="paw" />
-                <Text>Clients</Text>
-              </Button>
-              <Button vertical>
-                <Icon active name="medkit" />
-                <Text>Services</Text>
-              </Button>
-              <Button vertical>
-                <Icon name="person" />
-                <Text>Contact</Text>
-              </Button>
-            </FooterTab>
-          </Footer>
-        </Drawer>
-      </Container>
+              <Header
+                style={{
+                  backgroundColor: '#1ABC9C'
+                }}
+                androidStatusBarColor="#1ABC9C"
+              >
+                <Left>
+                  <Button
+                    rounded
+                    style={{ backgroundColor: '#1ABC9C' }}
+                    onPress={() =>
+                      this.setState({ showSidebar: !this.state.showSidebar })
+                    }
+                  >
+                    <Icon name="menu" color="#E8E8E8" />
+                  </Button>
+                </Left>
+                <Right>
+                  <Title
+                    style={{
+                      fontSize: 24,
+                      fontWeight: 'bold',
+                      color: '#E8E8E8',
+                      alignSelf: 'center'
+                    }}
+                  >
+                    Idyllic Paws
+                  </Title>
+                </Right>
+              </Header>
+              <Content
+                padder
+                style={{ alignContent: 'center', backgroundColor: '#A9E6DA' }}
+              >
+                <Card>
+                  <CardItem header bordered>
+                    <Text>Holistic Wellness for the Love of Cats & Dogs</Text>
+                  </CardItem>
+                  <CardItem bordered>
+                    <Body>
+                      <Text>
+                        Welcome to Idyllic Paws a house-call based, small animal
+                        practice dedicated to your animal companions and a
+                        holistic, individualized approach in patient healthcare.
+                        I’ve helped many dogs and cats overcome illnesses,
+                        allergies, and other immune disorders, and go on to lead
+                        happy, healthy lives.
+                      </Text>
+                    </Body>
+                  </CardItem>
+                  <CardItem footer bordered>
+                    <Text>Dr. Zenitsky looks forward to working with you!</Text>
+                  </CardItem>
+                </Card>
+              </Content>
+              <Footer>
+                <FooterTab
+                  style={{
+                    backgroundColor: '#1ABC9C',
+                    padding: 5
+                  }}
+                >
+                  <Button vertical>
+                    <Icon
+                      name="profile"
+                      type="AntDesign"
+                      style={{ fontSize: 23, color: '#E8E8E8' }}
+                    />
+                    <Text style={{ color: '#E8E8E8', fontSize: 10 }}>
+                      Files
+                    </Text>
+                  </Button>
+                  <Button vertical>
+                    <Icon
+                      name="paw"
+                      style={{ fontSize: 23, color: '#E8E8E8' }}
+                    />
+                    <Text style={{ color: '#E8E8E8', fontSize: 10 }}>
+                      Clients
+                    </Text>
+                  </Button>
+                  <Button vertical active>
+                    <Icon
+                      name="home"
+                      style={{ fontSize: 23, color: '#E8E8E8' }}
+                    />
+                    <Text style={{ color: '#E8E8E8', fontSize: 10 }}>Home</Text>
+                  </Button>
+                  <Button vertical>
+                    <Icon
+                      name="medkit"
+                      style={{ fontSize: 23, color: '#E8E8E8' }}
+                    />
+                    <Text style={{ color: '#E8E8E8', fontSize: 10 }}>
+                      Services
+                    </Text>
+                  </Button>
+                  <Button vertical>
+                    <Icon
+                      name="person"
+                      style={{ fontSize: 23, color: '#E8E8E8' }}
+                    />
+                    <Text style={{ color: '#E8E8E8', fontSize: 10 }}>
+                      Contact
+                    </Text>
+                  </Button>
+                </FooterTab>
+              </Footer>
+            </Drawer>
+          </Container>
+        }
+      </NavigationContainer>
     );
   }
 }
--- ipaws-app/components/Sidebar.tsx
+++ ipaws-app/components/Sidebar.tsx
@@ -29,16 +29,24 @@ export default class Sidebar extends Component {
               style={{
                 height: undefined,
                 width: undefined,
-                flex: 1
+                flex: 1,
+                borderRadius: 10
               }}
             />
           </View>
           <ListItem header first>
             <Text>Learn More</Text>
           </ListItem>
-          <ListItem icon>
+          <ListItem
+            icon
+            style={{
+              backgroundColor: '#70E6CF',
+              borderRadius: 10,
+              marginBottom: 5
+            }}
+          >
             <Left>
-              <Button style={{ backgroundColor: '#007AFF' }}>
+              <Button style={{ backgroundColor: '#40A9EF', margin: 10 }}>
                 <Icon active name="info" type="Feather" />
               </Button>
             </Left>
@@ -49,9 +57,16 @@ export default class Sidebar extends Component {
               <Icon name="arrow-forward" style={{ color: '#374650' }} />
             </Right>
           </ListItem>
-          <ListItem icon>
+          <ListItem
+            icon
+            style={{
+              backgroundColor: '#70E6CF',
+              borderRadius: 10,
+              marginBottom: 5
+            }}
+          >
             <Left>
-              <Button style={{ backgroundColor: '#007AFF' }}>
+              <Button style={{ backgroundColor: '#FF3030', margin: 10 }}>
                 <Icon active name="heart" type="Entypo" />
               </Button>
             </Left>
@@ -62,9 +77,16 @@ export default class Sidebar extends Component {
               <Icon active name="arrow-forward" style={{ color: '#374650' }} />
             </Right>
           </ListItem>
-          <ListItem icon>
+          <ListItem
+            icon
+            style={{
+              backgroundColor: '#70E6CF',
+              borderRadius: 10,
+              marginBottom: 5
+            }}
+          >
             <Left>
-              <Button style={{ backgroundColor: '#007AFF' }}>
+              <Button style={{ backgroundColor: '#CA79ED', margin: 10 }}>
                 <Icon active name="rate-review" type="MaterialIcons" />
               </Button>
             </Left>
@@ -75,9 +97,16 @@ export default class Sidebar extends Component {
               <Icon active name="arrow-forward" style={{ color: '#374650' }} />
             </Right>
           </ListItem>
-          <ListItem icon>
+          <ListItem
+            icon
+            style={{
+              backgroundColor: '#70E6CF',
+              borderRadius: 10,
+              marginBottom: 5
+            }}
+          >
             <Left>
-              <Button style={{ backgroundColor: '#007AFF' }}>
+              <Button style={{ backgroundColor: '#F4D03F', margin: 10 }}>
                 <Icon active name="question-answer" type="MaterialIcons" />
               </Button>
             </Left>
@@ -88,9 +117,22 @@ export default class Sidebar extends Component {
               <Icon active name="arrow-forward" style={{ color: '#374650' }} />
             </Right>
           </ListItem>
-          <ListItem icon>
+          <ListItem
+            icon
+            style={{
+              backgroundColor: '#70E6CF',
+              borderRadius: 10
+            }}
+          >
             <Left>
-              <Button style={{ backgroundColor: '#007AFF' }}>
+              <Button
+                style={{ backgroundColor: '#F77722', margin: 10 }}
+                onPress={() =>
+                  Linking.openURL(
+                    'https://seethewebdev.com/idyllicpaws.com/blog/'
+                  )
+                }
+              >
                 <Icon active name="blog" type="FontAwesome5" />
               </Button>
             </Left>
@@ -104,9 +146,13 @@ export default class Sidebar extends Component {
           <ListItem header first>
             <Text>Connect</Text>
           </ListItem>
-          <ListItem icon>
+          <ListItem
+            icon
+            style={{ backgroundColor: '#70E6CF', borderRadius: 10 }}
+          >
             <Left>
               <Button
+                style={{ backgroundColor: '#3B5998', margin: 10 }}
                 onPress={() =>
                   Linking.openURL('https://www.facebook.com/idyllicpawsvet')
                 }
